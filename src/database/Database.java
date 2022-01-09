edd38fce9c0bfb310fcccadd90ee1efb29342ed9
cyber-stefik <cyber-stefik@users.noreply.github.com>
2022-01-09 02:30:57 +0200

    Updated the database

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,98 +1,97 @@
 package database;
 
-import entities.AnnualChanges;
+import entities.AnnualChange;
 import entities.Child;
-import entities.Present;
+import entities.Gift;
 import fileio.Input;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
 public final class Database {
-	private int numberOfYears;
-	private Double santaBudget;
-	private ArrayList<Child> children = new ArrayList<>();
-	private ArrayList<Present> presents = new ArrayList<>();
-	private ArrayList<AnnualChanges> annualChanges = new ArrayList<>();
-	private static final Database instance = new Database();
-	private static int instanceCount = 0;
+    private int numberOfYears;
+    private Double santaBudget;
+    private ArrayList<Child> children = new ArrayList<>();
+    private ArrayList<Gift> gifts = new ArrayList<>();
+    private ArrayList<AnnualChange> annualChanges = new ArrayList<>();
+    private static final Database INSTANCE = new Database();
+    private static int instanceCount = 0;
 
-	private Database() {
-		instanceCount++;
-	}
+    private Database() {
+        instanceCount++;
+    }
 
-	public static Database getDatabase() {
-		return instance;
-	}
+    public static Database getDatabase() {
+        return INSTANCE;
+    }
 
-	public static int getNumberOfInstances() {
-		return instanceCount;
-	}
+    public static int getNumberOfInstances() {
+        return instanceCount;
+    }
 
-	public void addData(Input input) {
-		this.numberOfYears = input.getNumberOfYears();
-		this.santaBudget = input.getSantaBudget();
-		this.children = input.getChildren();
-		this.presents = input.getPresents();
-		this.annualChanges = input.getAnnualChanges();
-	}
+    public void addData(final Input input) {
+        this.numberOfYears = input.getNumberOfYears();
+        this.santaBudget = input.getSantaBudget();
+        this.children = input.getChildren();
+        this.gifts = input.getPresents();
+        this.annualChanges = input.getAnnualChanges();
+    }
 
-	public static int getInstanceCount() {
-		return instanceCount;
-	}
+    public static int getInstanceCount() {
+        return instanceCount;
+    }
 
-	public static void setInstanceCount(int instanceCount) {
-		Database.instanceCount = instanceCount;
-	}
+    public static void setInstanceCount(final int instanceCount) {
+        Database.instanceCount = instanceCount;
+    }
 
-	public ArrayList<Child> getChildren() {
-		return children;
-	}
+    public ArrayList<Child> getChildren() {
+        return children;
+    }
 
-	public ArrayList<Present> getPresents() {
-		return presents;
-	}
+    public ArrayList<Gift> getPresents() {
+        return gifts;
+    }
 
-	public ArrayList<AnnualChanges> getAnnualChanges() {
-		return annualChanges;
-	}
+    public ArrayList<AnnualChange> getAnnualChanges() {
+        return annualChanges;
+    }
 
-	public int getNumberOfYears() {
-		return numberOfYears;
-	}
+    public int getNumberOfYears() {
+        return numberOfYears;
+    }
 
-	public void setNumberOfYears(int numberOfYears) {
-		this.numberOfYears = numberOfYears;
-	}
+    public void setNumberOfYears(final int numberOfYears) {
+        this.numberOfYears = numberOfYears;
+    }
 
-	public Double getSantaBudget() {
-		return santaBudget;
-	}
+    public Double getSantaBudget() {
+        return santaBudget;
+    }
 
-	public void setSantaBudget(Double santaBudget) {
-		this.santaBudget = santaBudget;
-	}
+    public void setSantaBudget(final Double santaBudget) {
+        this.santaBudget = santaBudget;
+    }
 
-	public void setChildren(ArrayList<Child> children) {
-		this.children = children;
-	}
+    public void setChildren(final ArrayList<Child> children) {
+        this.children = children;
+    }
 
-	public void setPresents(ArrayList<Present> presents) {
-		this.presents = presents;
-	}
+    public void setPresents(final ArrayList<Gift> presents) {
+        this.gifts = presents;
+    }
 
-	public void setAnnualChanges(ArrayList<AnnualChanges> annualChanges) {
-		this.annualChanges = annualChanges;
-	}
+    public void setAnnualChanges(final ArrayList<AnnualChange> annualChanges) {
+        this.annualChanges = annualChanges;
+    }
 
-	@Override
-	public String toString() {
-		return "Database{" +
-				"numberOfYears=" + numberOfYears +
-				", santaBudget=" + santaBudget +
-				", children=" + children +
-				", presents=" + presents +
-				", annualChanges=" + annualChanges +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Database{"
+                + "numberOfYears=" + numberOfYears
+                + ", santaBudget=" + santaBudget
+                + ", children=" + children
+                + ", presents=" + gifts
+                + ", annualChanges=" + annualChanges
+                + '}';
+    }
 }
