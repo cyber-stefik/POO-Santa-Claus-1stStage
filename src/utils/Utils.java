987f465f0e46fc096df6c88b19bb2672db99569c
cyber-stefik <cyber-stefik@users.noreply.github.com>
2022-01-09 02:33:46 +0200

    Added an auxiliary function for tranforming a JSONArray to a String array

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -5,18 +5,18 @@ import org.json.simple.JSONArray;
 import java.util.ArrayList;
 
 public final class Utils {
-	public Utils() { }
+    private Utils() { }
 
-	public static ArrayList<String> convertJSONArray(final JSONArray array) {
-		if (array != null) {
-			ArrayList<String> finalArray = new ArrayList<>();
-			for (Object object : array) {
-				finalArray.add((String) object);
-			}
-			return finalArray;
-		} else {
-			return null;
-		}
-	}
+    public static ArrayList<String> convertJSONArray(final JSONArray array) {
+        if (array != null) {
+            ArrayList<String> finalArray = new ArrayList<>();
+            for (Object object : array) {
+                finalArray.add((String) object);
+            }
+            return finalArray;
+        } else {
+            return null;
+        }
+    }
 
 }
