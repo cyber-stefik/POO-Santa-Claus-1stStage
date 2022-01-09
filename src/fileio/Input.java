338eebfd027d5c40c844fbefce66379555b610b8
cyber-stefik <cyber-stefik@users.noreply.github.com>
2022-01-09 02:32:22 +0200

    Updated input strategy

--- src/fileio/Input.java
+++ src/fileio/Input.java
@@ -1,54 +1,54 @@
 package fileio;
 
-import entities.AnnualChanges;
+import entities.AnnualChange;
 import entities.Child;
-import entities.Present;
+import entities.Gift;
 
 import java.util.ArrayList;
 
 public final class Input {
-	private final int numberOfYears;
-	private final Double santaBudget;
-	private final ArrayList<Child> children;
-	private final ArrayList<Present> presents;
-	private final ArrayList<AnnualChanges> annualChanges;
+    private final int numberOfYears;
+    private final Double santaBudget;
+    private final ArrayList<Child> children;
+    private final ArrayList<Gift> gifts;
+    private final ArrayList<AnnualChange> annualChanges;
 
-	public Input() {
-		numberOfYears = -1;
-		santaBudget = -1.0;
-		children = null;
-		presents = null;
-		annualChanges = null;
-	}
+    public Input() {
+        numberOfYears = -1;
+        santaBudget = -1.0;
+        children = null;
+        gifts = null;
+        annualChanges = null;
+    }
 
-	public Input(final int numberOfYears, final Double santaBudget,
-				 final ArrayList<Child> children, final ArrayList<Present>
-				presents, final ArrayList<AnnualChanges> annualChanges) {
+    public Input(final int numberOfYears, final Double santaBudget,
+                 final ArrayList<Child> children, final ArrayList<Gift>
+                         gifts, final ArrayList<AnnualChange> annualChanges) {
 
-		this.numberOfYears = numberOfYears;
-		this.santaBudget = santaBudget;
-		this.children = children;
-		this.presents = presents;
-		this.annualChanges = annualChanges;
-	}
+        this.numberOfYears = numberOfYears;
+        this.santaBudget = santaBudget;
+        this.children = children;
+        this.gifts = gifts;
+        this.annualChanges = annualChanges;
+    }
 
-	public ArrayList<Child> getChildren() {
-		return children;
-	}
+    public ArrayList<Child> getChildren() {
+        return children;
+    }
 
-	public ArrayList<Present> getPresents() {
-		return presents;
-	}
+    public ArrayList<Gift> getPresents() {
+        return gifts;
+    }
 
-	public ArrayList<AnnualChanges> getAnnualChanges() {
-		return annualChanges;
-	}
+    public ArrayList<AnnualChange> getAnnualChanges() {
+        return annualChanges;
+    }
 
-	public int getNumberOfYears() {
-		return numberOfYears;
-	}
+    public int getNumberOfYears() {
+        return numberOfYears;
+    }
 
-	public Double getSantaBudget() {
-		return santaBudget;
-	}
+    public Double getSantaBudget() {
+        return santaBudget;
+    }
 }
--- src/fileio/InputLoader.java
+++ src/fileio/InputLoader.java
@@ -1,10 +1,10 @@
 package fileio;
 
 import common.Constants;
-import entities.AnnualChanges;
+import entities.AnnualChange;
 import entities.Child;
 import entities.ChildUpdate;
-import entities.Present;
+import entities.Gift;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -13,158 +13,143 @@ import utils.Utils;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public final class InputLoader {
     private final String inputPath;
 
     public InputLoader(final String inputPath) {
-		this.inputPath = inputPath;
-	}
+        this.inputPath = inputPath;
+    }
 
-	public String getInputPath() {
-		return inputPath;
-	}
+    public String getInputPath() {
+        return inputPath;
+    }
 
-	public Input readData() {
-		JSONParser jsonParser = new JSONParser();
-		String numberOfYears = null;
-		String santaBudget = null;
-		ArrayList<Child> children = new ArrayList<>();
-		ArrayList<Present> santaGiftsList = new ArrayList<>();
-		ArrayList<AnnualChanges> annualChanges = new ArrayList<>();
-		ArrayList<Present> newGifts = new ArrayList<>();
-		ArrayList<Child> newChildren = new ArrayList<>();
-		ArrayList<ChildUpdate> childUpdates = new ArrayList<>();
+    public Input readData() {
+        JSONParser jsonParser = new JSONParser();
+        String numberOfYears = "0";
+        String santaBudget = "0";
+        ArrayList<Child> children = new ArrayList<>();
+        ArrayList<Gift> santaGiftsList = new ArrayList<>();
+        ArrayList<AnnualChange> annualChanges = new ArrayList<>();
 
-		try {
-			JSONObject jsonObject = (JSONObject) jsonParser
-					.parse(new FileReader(inputPath));
-			numberOfYears = String.valueOf((jsonObject).
-							get(Constants.NUMBEROFYEARS));
-			santaBudget = String.valueOf((jsonObject).
-							get(Constants.SANTABUDGET));
-			JSONObject initialData = (JSONObject) jsonObject.
-									get(Constants.INITIALDATA);
-			JSONArray jsonAnnualChanges = (JSONArray) jsonObject.
-									get(Constants.ANNUALCHANGES);
-			JSONArray jsonChildren = (JSONArray)
-					initialData.get(Constants.CHILDREN);
-			JSONArray jsonPresents = (JSONArray)
-					initialData.get(Constants.PRESENTS);
+        try {
+            JSONObject jsonObject = (JSONObject) jsonParser
+                    .parse(new FileReader(inputPath));
+            numberOfYears = String.valueOf((jsonObject).
+                    get(Constants.NUMBEROFYEARS));
+            santaBudget = String.valueOf((jsonObject).
+                    get(Constants.SANTABUDGET));
+            JSONObject initialData = (JSONObject) jsonObject.
+                    get(Constants.INITIALDATA);
+            JSONArray jsonAnnualChanges = (JSONArray) jsonObject
+                    .get(Constants.ANNUALCHANGES);
+            JSONArray jsonChildren = (JSONArray)
+                    initialData.get(Constants.CHILDREN);
+            JSONArray jsonPresents = (JSONArray)
+                    initialData.get(Constants.PRESENTS);
 
-			if (jsonChildren != null) {
-				for (Object jsonChild : jsonChildren) {
-					createChild(children, (JSONObject) jsonChild);
-				}
-			} else {
-				System.out.println("Nu exista copii");
-			}
+            if (jsonChildren != null) {
+                for (Object jsonChild : jsonChildren) {
+                    createChild(children, (JSONObject) jsonChild);
+                }
+            } else {
+                System.out.println("Nu exista copii");
+            }
 
-			if (jsonPresents != null) {
-				createGifts(santaGiftsList, jsonPresents);
-			} else {
-				System.out.println("Nu exista cadouri");
-			}
+            if (jsonPresents != null) {
+                createGifts(santaGiftsList, jsonPresents);
+            } else {
+                System.out.println("Nu exista cadouri");
+            }
+            for (Object annualChange : jsonAnnualChanges) {
+                ArrayList<Gift> newGifts = new ArrayList<>();
+                ArrayList<Child> newChildren = new ArrayList<>();
+                ArrayList<ChildUpdate> childUpdates = new ArrayList<>();
+                String newSantaBudget = String.valueOf(((JSONObject)
+                        annualChange).get(Constants.NEWSANTABUDGET));
+                JSONArray jsonNewGifts = ((JSONArray) ((JSONObject)
+                        annualChange).get(Constants.NEWGIFTS));
+                JSONArray jsonNewChildren = ((JSONArray) ((JSONObject)
+                        annualChange).get(Constants.NEWCHILDREN));
+                JSONArray jsonChildrenUpdates = ((JSONArray) ((JSONObject)
+                        annualChange).get(Constants.CHILDRENUPDATES));
 
-			if (jsonAnnualChanges != null) {
-				for (Object annualChange : jsonAnnualChanges) {
-					String newSantaBudget = String.valueOf(((JSONObject)
-							annualChange).get(Constants.NEWSANTABUDGET));
-					JSONArray jsonNewGifts = ((JSONArray) ((JSONObject)
-							annualChange).get(Constants.NEWGIFTS));
-					JSONArray jsonNewChildren = ((JSONArray) ((JSONObject)
-							annualChange).get(Constants.NEWCHILDREN));
-					JSONArray jsonChildrenUpdates = ((JSONArray) ((JSONObject)
-							annualChange).get(Constants.CHILDRENUPDATES));
+                createGifts(newGifts, jsonNewGifts);
+                for (Object jsonChild : jsonNewChildren) {
+                    createChild(newChildren, (JSONObject) jsonChild);
+                }
+                for (Object jsonChild : jsonChildrenUpdates) {
+                    createChildUpdate(childUpdates, (JSONObject) jsonChild);
+                }
+                annualChanges.add(new AnnualChange(
+                        Double.valueOf(newSantaBudget),
+                        newGifts, newChildren, childUpdates));
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+        return new Input(Integer.parseInt(numberOfYears),
+                Double.valueOf(santaBudget), children, santaGiftsList,
+                annualChanges);
+    }
 
-					if (jsonNewGifts != null) {
-						createGifts(newGifts, jsonNewGifts);
-					} else {
-						System.out.println("Nu exista cadouri noi");
-					}
+    private void createChildUpdate(final ArrayList<ChildUpdate> childUpdates,
+                           final JSONObject jsonChild) {
+        String id = String.valueOf((jsonChild)
+                .get(Constants.ID));
+        String niceScore = String.valueOf(((JSONObject)
+                jsonChild).get(Constants.NICESCORE));
+        if (niceScore == "null") {
+            niceScore = "-1";
+        }
 
-					if (jsonNewChildren != null) {
-						for (Object jsonChild : jsonNewChildren) {
-							createChild(newChildren, (JSONObject) jsonChild);
-						}
-					} else {
-						System.out.println("Nu exista copii noi");
-					}
+        childUpdates.add(new ChildUpdate(
+                Integer.parseInt(id),
+                Double.valueOf(niceScore),
+                Utils.convertJSONArray((JSONArray)
+                        jsonChild.get(Constants.GIFTSPREFERENCES))));
+    }
 
-					if (jsonChildrenUpdates != null) {
-						for (Object jsonChild : jsonChildrenUpdates) {
-							String id = String.valueOf(((JSONObject) jsonChild)
-										.get(Constants.ID));
-							String niceScore = String.valueOf(((JSONObject)
-									jsonChild).get(Constants.NICESCORE));
-							if (niceScore.equals("null")) {
-								niceScore = "-1";
-							}
-							childUpdates.add(new ChildUpdate(
-										Integer.parseInt(id),
-										Double.valueOf(niceScore),
-										Utils.convertJSONArray((JSONArray)
-										((JSONObject) jsonChild).
-										get(Constants.CHILDRENUPDATES))));
-						}
-					} else {
-						System.out.println("Nu exista update-uri");
-					}
+    private void createGifts(final ArrayList<Gift> newGifts,
+                             final JSONArray jsonNewGifts) {
+        for (Object newGift : jsonNewGifts) {
+            String productName = String.valueOf(((JSONObject) newGift)
+                    .get(Constants.PRODUCTNAME));
+            String price = String.valueOf(((JSONObject) newGift)
+                    .get(Constants.PRICE));
+            String category = String.valueOf(((JSONObject) newGift)
+                    .get(Constants.CATEGORY));
+            newGifts.add(new Gift(productName,
+                    Double.valueOf(price),
+                    category));
+        }
+    }
 
-					annualChanges.add(new AnnualChanges(
-							Double.valueOf(newSantaBudget),
-							newGifts, newChildren, childUpdates));
-				}
-			}
-
-		} catch (ParseException | IOException e) {
-			e.printStackTrace();
-		}
-		assert numberOfYears != null;
-		assert santaBudget != null;
-		return new Input(Integer.parseInt(numberOfYears),
-				Double.valueOf(santaBudget), children, santaGiftsList,
-				annualChanges);
-	}
-
-	private void createGifts(ArrayList<Present> newGifts, JSONArray jsonNewGifts) {
-		for (Object newGift : jsonNewGifts) {
-			String productName = String.valueOf(((JSONObject) newGift)
-					.get(Constants.PRODUCTNAME));
-			String price = String.valueOf(((JSONObject) newGift)
-					.get(Constants.PRICE));
-			String category = String.valueOf(((JSONObject) newGift)
-					.get(Constants.CATEGORY));
-			newGifts.add(new Present(productName,
-									Double.valueOf(price),
-									category));
-		}
-	}
-
-	private void createChild(ArrayList<Child> children, JSONObject jsonChild) {
-		String id = String.valueOf(jsonChild
-				.get(Constants.ID));
-		String lastName = String.valueOf(jsonChild
-				.get(Constants.LASTNAME));
-		String firstName = String.valueOf(jsonChild
-				.get(Constants.FIRSTNAME));
-		String age = String.valueOf(jsonChild
-				.get(Constants.AGE));
-		String city = String.valueOf(jsonChild
-				.get(Constants.CITY));
-		String niceScore = String.valueOf(jsonChild
-				.get(Constants.NICESCORE));
-		children.add(new Child(
-				Integer.parseInt(id),
-				lastName,
-				firstName,
-				Integer.parseInt(age),
-				city,
-				Double.valueOf(niceScore),
-				Utils.convertJSONArray((JSONArray)
-						jsonChild.get(Constants.GIFTSPREFERENCES)
-		)));
-	}
+    private void createChild(final ArrayList<Child> children,
+                             final JSONObject jsonChild) {
+        String id = String.valueOf(jsonChild
+                .get(Constants.ID));
+        String lastName = String.valueOf(jsonChild
+                .get(Constants.LASTNAME));
+        String firstName = String.valueOf(jsonChild
+                .get(Constants.FIRSTNAME));
+        String age = String.valueOf(jsonChild
+                .get(Constants.AGE));
+        String city = String.valueOf(jsonChild
+                .get(Constants.CITY));
+        String niceScore = String.valueOf(jsonChild
+                .get(Constants.NICESCORE));
+        children.add(new Child(
+                Integer.parseInt(id),
+                lastName,
+                firstName,
+                Integer.parseInt(age),
+                city,
+                Double.valueOf(niceScore),
+                Utils.convertJSONArray((JSONArray)
+                        jsonChild.get(Constants.GIFTSPREFERENCES)
+                )));
+    }
 }
